ba1bfe847ae5a9699f85084078a0e42a8508aba3
Rishav Bharti <rishavbharti21@gmail.com>
2020-04-24 16:53:05 +0530

    Build

---
+++ build/precache-manifest.82f6fd4dd41372fff88d9319455a45de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c669d8e6fc7d6db83f66",
+    "url": "/static/css/main.bc6ba0cd.chunk.css"
+  },
+  {
+    "revision": "c669d8e6fc7d6db83f66",
+    "url": "/static/js/main.c669d8e6.chunk.js"
+  },
+  {
+    "revision": "ed3cce13a3432c34c207",
+    "url": "/static/js/1.ed3cce13.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "cd903a2fb8d67792a7fd1ab224dcf421",
+    "url": "/index.html"
+  }
+];
